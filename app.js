7610fd95d936a18b189aeadffc939f1a9e02ca22
nebdil <dilannebioglu@gmail.com>
2017-12-11 14:49:49 -0500

    4 tests done
    wq
    :wq

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-let convert = {};
+let algo = {};
 
-convert.cToF = function(celsius) {
-  if(!Number.isInteger(celsius)) return undefined;
-  return celsius * 9 / 5 + 32;
+// Using the JavaScript language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+algo.LongestWord = function(sen) {
+  let longest = sen.match(/[\w]+/g).sort(function(a, b) {
+    return b.length - a.length
+  }).shift();
+  return longest;
 }
 
-convert.fToC = function(fahrenheit) {
-  if(!Number.isInteger(fahrenheit)) return undefined;
-  return (fahrenheit - 32) * 5 / 9;
+// Using the JavaScript language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+algo.FirstFactorial = function(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i
+  }
+  return fact;
 }
 
-module.exports = convert;
+//Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+algo.FirstReverse = function(str) {
+  return str.split('').reverse().join('');
+}
+
+//Using the JavaScript language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+algo.LetterChanges = function(str) {
+  let arr = str.split('');
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].match(/^[A-Za-z]+$/)) {
+      arr[i].replace((l) => {
+        console.log('in replace')
+
+        let modified = '';
+        let alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+        for (let j = 0; j < alphabet.length; j++) {
+          if (l === alphabet[j]) {
+            modified = alphabet[j+1]
+          }
+        }
+        if(modified === 'a' || modified === 'e' || modified === 'i' || modified === 'o' || modified === 'u') {
+          modified.uppercase()
+        }
+        return modified;
+      })
+    }
+  }
+  return arr.join('');
+}
+
+module.exports = algo;
--- test/test.js
+++ test/test.js
@@ -1,27 +1,42 @@
-let convert = require('../app.js')
+let algo = require('../app.js');
 let assert = require('assert');
 
-describe('Temperature Conversion', function() {
-  describe('cToF', function() {
-    it('should convert -40 celsius to -40 fahrenheit', function() {
-      assert.equal(-40, convert.cToF(-40));
+describe('Algorithms', function() {
+
+  describe('Longest word in a string', function() {
+    it('should return the first longest word in a string, ignores special characters', function() {
+      assert.equal('time', algo.LongestWord('fun&!! time'));
     });
-    it('should convert 0 celsius to 32 fahrenheit', function() {
-      assert.equal(32, convert.cToF(0));
-    });
-    it('should return undefined if no temperature is input', function() {
-      assert.equal(undefined, convert.cToF(''));
+    it('should return the first longest word in a string, ignores special characters', function() {
+      assert.equal('love', algo.LongestWord('I love dogs'));
     });
   });
-  describe('fToC', function() {
-    it('should convert -40 fahrenheit to -40 celsius', function() {
-      assert.equal(-40, convert.fToC(-40));
+
+  describe('factorial', function() {
+    it('should return 24', function() {
+      assert.equal(24, algo.FirstFactorial(4));
     });
-    it('should convert 32 fahrenheit to 0 celsius', function() {
-      assert.equal(0, convert.fToC(32));
-    });
-    it('should return undefined if no temperature is input', function() {
-      assert.equal(undefined, convert.fToC('a'));
+    it('should return 40320', function() {
+      assert.equal(40320, algo.FirstFactorial(8));
     });
   });
+
+  describe('reverse', function() {
+    it('should return etybredoc', function() {
+      assert.equal('etybredoc', algo.FirstReverse('coderbyte'));
+    });
+    it('should return I Love Code', function() {
+      assert.equal('I Love Code', algo.FirstReverse('edoC evoL I'));
+    });
+  });
+
+  describe('modified string', function() {
+    it('should return Ifmmp*3', function() {
+      assert.equal('Ifmmp*3', algo.LetterChanges('hello*3'));
+    });
+    it('should return gvO Ujnft!', function() {
+      assert.equal('gvO Ujnft!', algo.LetterChanges('fun times!'));
+    });
+  });
+
 });
