1540f1cfefd432add555338c2ff260f3f014381f
nebdil <dilannebioglu@gmail.com>
2017-12-13 23:51:27 -0500

    simple symbols added

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ let algo = {};
 
 // Using the JavaScript language, have the function SimpleSymbols(str) take the str parameter being passed and determine if it is an acceptable sequence by either returning the string true or false. The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol. So the string to the left would be false. The string will not be empty and will have at least one letter.
 algo.SimpleSymbols = function(str) {
-  
+  let arr = str.split('')
+  let booleanArr = []
+  arr.forEach((e, i, a) => {
+    if (a[i - 1] === '+' && a[i + 1] === '+') {
+      booleanArr.push(true)
+    }
+  })
+  return (booleanArr.length === str.match(/[a-z]/gi).length)
 }
 
 // Using the JavaScript language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
